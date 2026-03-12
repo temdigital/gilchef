09a6c2cfaed4de83fb932e652e722289de1438d3
temdigital <temnoentornosul@gmail.com>
2026-03-12 13:55:31 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,20 +1,33 @@
-// animação suave ao rolar a página
+const menuToggle = document.getElementById("menuToggle")
+const navMenu = document.getElementById("navMenu")
 
-const elements = document.querySelectorAll(".card");
+menuToggle.addEventListener("click", function(e){
 
-window.addEventListener("scroll",()=>{
+e.stopPropagation()
+navMenu.classList.toggle("active")
 
-elements.forEach(el=>{
+})
 
-const pos = el.getBoundingClientRect().top;
+/* fechar menu ao clicar no link */
 
-if(pos < window.innerHeight - 100){
+document.querySelectorAll("#navMenu a").forEach(link=>{
 
-el.style.opacity = "1";
-el.style.transform = "translateY(0)";
+link.addEventListener("click",()=>{
+
+navMenu.classList.remove("active")
+
+})
+
+})
+
+/* fechar menu clicando fora */
+
+document.addEventListener("click",function(event){
+
+if(!navMenu.contains(event.target) && !menuToggle.contains(event.target)){
+
+navMenu.classList.remove("active")
 
 }
 
-});
-
-});
+})
